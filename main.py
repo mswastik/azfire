204bbce79e3b71fbd31a88ccf58cf224c9586897
Swastik <m.swastik@gmail.com>
2024-10-01 09:48:04 +0530

    created fc form

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
+@app.get("/reg")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/clf")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/clt")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/fc")
+async def index(request: Request):
+    return templates.TemplateResponse("fc.html", {"request": request})
+
+@app.post("/fc")
+async def index(request: Request):
+    item = await request.form()
+    print(item.items())
+    return item
