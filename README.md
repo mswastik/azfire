b7cbc8b337041b8c8fa4fbb4cb3376f3b01443c8
Swastik <m.swastik@gmail.com>
2024-10-01 20:46:37 +0530

    implemented regression

---
+++ README.md
@@ -0,0 +1,2 @@
+# fire
+Fire Forecasting https://ml-b0fkfgb8f7fqb6bz.canadacentral-01.azurewebsites.net/
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+import polars as pl
+import pickle
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -12,7 +15,7 @@ async def index(request: Request):
 
 @app.get("/reg")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("reg.html", {"request": request})
 
 @app.get("/clf")
 async def index(request: Request):
@@ -24,10 +27,22 @@ async def index(request: Request):
 
 @app.get("/fc")
 async def index(request: Request):
-    return templates.TemplateResponse("fc.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/fc")
+@app.post("/reg")
 async def index(request: Request):
     item = await request.form()
-    print(item.items())
-    return item
+    #print(item.items())
+    def predf(item):
+        dft=pl.DataFrame([[item.get('day'), item.get('month'), item.get('year'), item.get('season'), item.get('holiday'), item.get('weekday'), item.get('workingday'),
+                        item.get('weathersit'), item.get('temp'), item.get('atemp'), item.get('hum'), item.get('windspeed')]],schema=['day', 'mnth', 'year', 'season', 'holiday', 'weekday', 'workingday',
+                        'weathersit', 'temp', 'atemp', 'hum', 'windspeed'])
+        with open('bike.pkl', 'rb') as f:
+            reg=pickle.load(f)   
+        return reg.predict(dft)        
+    p=predf(item)
+    return HTMLResponse('<div class="text-md p-5 bg-gray-300 rounded-md" id="result">The predicted no. of rentals are {}</div>'.format(int(p[0])))
+
+@app.get("/contact")
+async def index(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
